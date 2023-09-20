c3e316b4f27623155f4b9c0da41ff61d99d71bae
kylethmas <97603912+kylethmas@users.noreply.github.com>
2023-09-20 19:26:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Author Kyle Thomas
+
+// Smooth Scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle Nav menu on mobiles
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+
+
+
+
